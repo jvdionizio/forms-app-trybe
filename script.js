ce1977135e5a4be7def405801da688316886b969
jvdionizio <dionizio145@gmail.com>
2022-01-11 15:48:32 -0300

    requisitos 2 e 3 adicionados

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 window.onload = function pgCarregada() {
-  
+  const btnLogin = document.querySelector('#btn-login');
+
+  function login(){
+    const validEmail = "tryber@teste.com"
+    const validSenha = "123456"
+    const inputEmail = document.querySelector("#email").value;
+    const inputSenha = document.querySelector("#senha").value;
+    if (inputEmail === validEmail && inputSenha === validSenha) {
+      alert("Olá, Tryber!")
+    }
+    else{
+      alert("Email ou senha inválidos.")
+    }
+  }
+
+  btnLogin.addEventListener('click', login)
+
 }
