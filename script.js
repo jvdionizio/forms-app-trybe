7debc4079ac82c073481a1c9b2e6e7349b273461
jvdionizio <dionizio145@gmail.com>
2022-01-11 21:53:28 -0300

    Arrumando lint

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-/* eslint-disable max-lines-per-function */
-window.onload = function pgCarregada() {
-  const btnLogin = document.querySelector('#btn-login');
+const btnLogin = document.querySelector('#btn-login');
 
-  function login() {
-    const validEmail = 'tryber@teste.com';
-    const validSenha = '123456';
-    const inputEmail = document.querySelector('#email').value;
-    const inputSenha = document.querySelector('#senha').value;
-    if (inputEmail === validEmail && inputSenha === validSenha) {
-      alert('Olá, Tryber!');
-    } else {
-      alert('Email ou senha inválidos.');
-    }
+function login() {
+  const validEmail = 'tryber@teste.com';
+  const validSenha = '123456';
+  const inputEmail = document.querySelector('#email').value;
+  const inputSenha = document.querySelector('#senha').value;
+  if (inputEmail === validEmail && inputSenha === validSenha) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
   }
+}
 
-  btnLogin.addEventListener('click', login);
+btnLogin.addEventListener('click', login);
 
-  function Submit() {
-    const btnSubmit = document.getElementById('submit-btn');
-    const agreement = document.getElementById('agreement');
+function Submit() {
+  const btnSubmit = document.getElementById('submit-btn');
+  const agreement = document.getElementById('agreement');
 
-    if (agreement.checked === false) {
+  if (agreement.checked === false) {
+    btnSubmit.disabled = true;
+  }
+  function acceptAgreement() {
+    if (agreement.checked === true) {
+      btnSubmit.disabled = false;
+    } else {
       btnSubmit.disabled = true;
     }
-    function acceptAgreement() {
-      if (agreement.checked === true) {
-        btnSubmit.disabled = false;
-      } else {
-        btnSubmit.disabled = true;
-      }
-    }
-    agreement.addEventListener('click', acceptAgreement);
   }
+  agreement.addEventListener('click', acceptAgreement);
+}
 
-  Submit();
+Submit();
 
-  const textArea = document.querySelector('#textarea');
+const textArea = document.querySelector('#textarea');
 
-  function contar() {
-    const contador = document.getElementById('counter');
-    const tamanho = document.querySelector('#textarea').value.length;
-    const counterValue = 500 - tamanho;
+function contar() {
+  const contador = document.getElementById('counter');
+  const tamanho = document.querySelector('#textarea').value.length;
+  const counterValue = 500 - tamanho;
 
-    contador.innerText = counterValue;
-  }
+  contador.innerText = counterValue;
+}
 
-  textArea.addEventListener('keyup', contar);
-};
+textArea.addEventListener('keyup', contar);
