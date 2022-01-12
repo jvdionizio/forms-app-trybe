0fb558a1c5f2b76f3054029275cf71c5d8845811
Luiz Guilherme Avelino <guilhermerx18@gmail.com>
2022-01-12 10:33:42 -0300

    test

--- script.js
+++ script.js
@@ -1,46 +1,86 @@
-const btnLogin = document.querySelector('#btn-login');
+window.onload = function pgCarregada() {
+  const btnLogin = document.querySelector('#btn-login');
 
-function login() {
-  const validEmail = 'tryber@teste.com';
-  const validSenha = '123456';
-  const inputEmail = document.querySelector('#email').value;
-  const inputSenha = document.querySelector('#senha').value;
-  if (inputEmail === validEmail && inputSenha === validSenha) {
-    alert('Olá, Tryber!');
-  } else {
-    alert('Email ou senha inválidos.');
-  }
-}
-
-btnLogin.addEventListener('click', login);
-
-function Submit() {
-  const btnSubmit = document.getElementById('submit-btn');
-  const agreement = document.getElementById('agreement');
-
-  if (agreement.checked === false) {
-    btnSubmit.disabled = true;
-  }
-  function acceptAgreement() {
-    if (agreement.checked === true) {
-      btnSubmit.disabled = false;
+  function login() {
+    const validEmail = 'tryber@teste.com';
+    const validSenha = '123456';
+    const inputEmail = document.querySelector('#email').value;
+    const inputSenha = document.querySelector('#senha').value;
+    if (inputEmail === validEmail && inputSenha === validSenha) {
+      alert('Olá, Tryber!');
     } else {
-      btnSubmit.disabled = true;
+      alert('Email ou senha inválidos.');
     }
   }
-  agreement.addEventListener('click', acceptAgreement);
+
+  btnLogin.addEventListener('click', login);
+
+  function btnSubmit () {
+    const btnSubmit = document.getElementById('submit-btn')
+    const agreement = document.getElementById('agreement')
+
+    if (agreement.checked == false) {
+      btnSubmit.disabled = true;
+    } 
+    
+    agreement.addEventListener('click', acceptAgreement)
+
+    function acceptAgreement() {
+      if(agreement.checked == true){
+        btnSubmit.disabled = false;
+      } else {
+        btnSubmit.disabled = true;
+      }
+    }
+
+  }
+
+  
+  btnSubmit()
+
+};
+
+function obterDados() {
+  dados = {}
+
+  let inputName = document.querySelector('#input-name').value
+  let inputLastName = document.querySelector('#input-lastname').value
+  let inputEmail = document.querySelector('#input-email').value
+  let inputHouse = document.querySelector('#house').value
+  let inputFamily = ''
+
+  let inputSkill = document.getElementsByClassName('subject')
+
+  let inputFamily1 = document.querySelector('#family1')
+  let inputFamily2 = document.querySelector('#family2')
+  let inputFamily3 = document.querySelector('#family3')
+
+  if(inputFamily1.checked){
+    inputFamily = inputFamily1.value
+
+  } else if (inputFamily2.checked){
+    inputFamily = inputFamily2.value
+
+  } else if (inputFamily3.checked){
+    inputFamily = inputFamily3.value
+  }
+
+  let valores = []
+  
+  for(let index = 0; index < inputSkill.length; index++){
+    if(index.checked){
+      console.log(index.value)
+    }
+  }
+
+
+
+  
+  dados.Nome = `${inputName}` + ' ' + `${inputLastName}`
+  dados.Email = `${inputEmail}`
+  dados.House = `${inputHouse}`
+  dados.Família = inputFamily
+  dados.Matérias = ''
+  
+  return valores
 }
-
-Submit();
-
-const textArea = document.querySelector('#textarea');
-
-function contar() {
-  const contador = document.getElementById('counter');
-  const tamanho = document.querySelector('#textarea').value.length;
-  const counterValue = 500 - tamanho;
-
-  contador.innerText = counterValue;
-}
-
-textArea.addEventListener('keyup', contar);
